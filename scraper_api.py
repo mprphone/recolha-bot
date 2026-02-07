1aad3b08fb276499cfc6fe8ef6d238fd8c6fcd58
mprphone <mpr.phone@gmail.com>
2026-02-07 09:17:22 +0000

    Refactor scraper_api.py for improved readability

--- scraper_api.py
+++ scraper_api.py
@@ -122,3 +122,4 @@ if __name__ == '__main__':
     # O Render atribui uma porta automaticamente na variável PORT
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
+
